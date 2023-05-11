2d6f58802c9c0a4b901cc8139cc0fe6be0da0e93
rruiz103 <raymondjohn.ruiz16@gmail.com>
2023-05-10 20:41:23 -0400

    Using For Loops

--- GitHub Practice Solution/GitHub Practice Project/Program.cs
+++ GitHub Practice Solution/GitHub Practice Project/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello John, the wonderful world of github!");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Iteration Number {i}");
+
+}

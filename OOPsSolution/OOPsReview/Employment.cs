ec124b6be70c01c4ec1750173a441b0daf9bbfdb
rruiz103 <raymondjohn.ruiz16@gmail.com>
2023-05-15 20:09:01 -0400

    Consructing Properties and Adding Utilities Method / Overloading Methods

--- OOPsSolution/OOPsReview/Employment.cs
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -32,7 +32,7 @@
             get { return _Title; }
 
             // set = mutator / it is within the set that the validation of the data  is done
-            //      determine if the date is accessible
+            //      determine if the data is accessible
 
             set 
             {
@@ -48,6 +48,62 @@
             }
         }
 
+        public SupervisoryLevel Level
+        { 
+                get { return _Level; }
+                private set
+                {
+
+                    // a private set means that the property can only be set
+                    // by code within the class definition
+                    // advantage of doing this is increasing the security on the
+                    // field as any change is under the control of the class definition
+
+                    // Enum
+                    // Validate that the value given as an enum is actually defined
+                    // A user of this class could send in an integer value that was type casted as this enum data type but have a 
+                    // none defined value
+                    // to test an enum value use Enum.IsDefnied(typeof()}, value)
+                    // where xxx is thge name of the enum datatype
+
+                    if (!Enum.IsDefined(typeof(SupervisoryLevel), value)) 
+                    {
+                        throw new ArgumentException($"Supervisory Level is invalid: {value}");
+                    }
+                    _Level = value;
+                }
+        } // End of Level Property
+
+        // Validate the years of service in the employee position as a positive value
+
+        public double Years
+        {
+                get { return _Years; } // used on the right side of an assignment statement or in an expression
+                set                    // used on the left side of an assignment statement 
+                {
+                    // if (value < 0)      // using Utilties generic method to do this test
+                    if (!Utilities.IsZeroOrPositive(value))
+                    { 
+                        throw new ArgumentOutOfRangeException("value");
+                    }
+                    _Years = value;
+                }
+        } // End of Years Property
+
+        // this property is an example of an auto implemented property
+        // there is no validation within the property
+        // there is no private data member for this property
+        // the system will generate an internal storage area for the data
+        // and handle the setting and getting from that storage area
+        // the property will only be able to be set via a constructor or a method
+        // auto implemented properties can have either a public or private set
+        // using a public or private set is a design decision
+
+        public DateTime StartDate
+        {
+            get;
+            private set;
+        } // End of StartDate Property
 
     } // End Employment Class
 
---
+++ OOPsSolution/OOPsReview/Utilities.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public static class Utilities
+    {
+        // static class are not instantiated by the outside user (developer/code)
+        // static class items are referenced using: classname.xxxx
+        // methods within this class have the keyword static in their signature
+        // static means shared / it is shared between all outside users at the same time
+        // do not consider saving data within a static class because you cannot be certain 
+        // it will be there when you use the class again
+        // use-only type of class
+        // consider placing generic re-usable methods with a static class
+
+        // Sample of a generic method: numeric is a zero or positive value
+
+        public static bool IsZeroOrPositive(double value)
+        {
+            // a structure method (apply to loops, etc) will have a single entry point 
+            // and a single exit point
+            // in this course you will avoid where possible multiple returns from a method
+            // in this course you will avoid using a break to exit a loop structure or if structure
+
+            bool valid = true;
+            if (value < 0.0) 
+            { 
+                valid = false;
+            }
+            return valid;
+
+        } // End of IsZeroOrPositive Method
+
+        // overload the IsZeroOrPositive so that it uses integers or decimals
+
+        public static bool IsZeroOrPositive(int value)
+        {
+            // a structure method (apply to loops, etc) will have a single entry point 
+            // and a single exit point
+            // in this course you will avoid where possible multiple returns from a method
+            // in this course you will avoid using a break to exit a loop structure or if structure
+
+            bool valid = true;
+            if (value < 0)
+            {
+                valid = false;
+            }
+            return valid;
+
+        } // End of IsZeroOrPositive Method
+
+        public static bool IsZeroOrPositive(decimal value)
+        {
+            // a structure method (apply to loops, etc) will have a single entry point 
+            // and a single exit point
+            // in this course you will avoid where possible multiple returns from a method
+            // in this course you will avoid using a break to exit a loop structure or if structure
+
+            bool valid = true;
+            if (value < 0.0m)
+            {
+                valid = false;
+            }
+            return valid;
+
+        } // End of IsZeroOrPositive Method
+
+    }
+}

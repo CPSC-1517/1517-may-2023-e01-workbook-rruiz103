3bd5322aa400cf89f67a7e4f10952935813f3580
rruiz103 <raymondjohn.ruiz16@gmail.com>
2023-05-24 19:46:14 -0400

    Introduction to the Record Data Type

---
+++ OOPsSolution/OOPsReview/Residence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public record Residence (int Number, string Street, string City, string Province, string PostalCode)
+    {
+        public override string ToString()
+        {
+            return $"{Number},{Street},{City},{Province},{PostalCode}";
+        } // End of ToString Method
+
+    } // End of Residence Class
+} // End of OOPsReview namespace
---
+++ OOPsSolution/Sandbox/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPsReview;
+
+Console.WriteLine("Hello, World!");
+
+Residence myHome = new Residence(123,"Maple St.","Edmonton","AB","T6Y7U8");
+Console.WriteLine(myHome.ToString());
+
+//Can I change the value in the record instance?
+// myHome.Number = 321;
+// it's read only, so the answer is No.
+// To alter a value in tjhe record instance, you must create a new instance
+
+myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());

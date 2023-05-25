74d350159fa2116650c06e59681a3edd2d8f98f1
rruiz103 <raymondjohn.ruiz16@gmail.com>
2023-05-24 20:19:40 -0400

    Refractory Introduction

--- OOPsSolution/Sandbox/Program.cs
+++ OOPsSolution/Sandbox/Program.cs
@@ -13,3 +13,57 @@ Console.WriteLine(myHome.ToString());
 
 myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
 Console.WriteLine(myHome.ToString());
+
+// Exanple of Refractoring 
+// Refractoring is the process of restructing code, while not
+// changing its original functionality. The goal is to improve internal
+// code by making small changes without altering the codes external behavior
+
+// Original Code
+bool flag = false;
+if (myHome.Province.ToLower() == "ab")
+{ 
+    flag= true;
+}
+
+if (myHome.Province.ToLower() == "bc")
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower() == "sk")
+{
+    flag = true;
+}
+
+if (myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
+
+// Refractor using a switch statement
+
+if (myHome.Province.ToLower() == "ab" || myHome.Province.ToLower() == "bc" || myHome.Province.ToLower() == "sk" || myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
+
+
+switch (myHome.Province.ToLower())
+{
+    case "ab":
+    case "bc":
+    case "sk":
+    case "mn":
+        { 
+            flag = true;
+            break;
+        }
+    default:
+        { 
+         flag= false;
+         break;
+        }
+     
+
+}

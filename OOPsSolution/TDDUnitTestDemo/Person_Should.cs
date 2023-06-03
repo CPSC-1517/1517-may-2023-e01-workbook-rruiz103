68c82c3ec328b9011c641c493bf788c7202677aa
rruiz103 <raymondjohn.ruiz16@gmail.com>
2023-06-02 20:28:42 -0400

    Clean up of Unit Test Codes

--- OOPsSolution/TDDUnitTestDemo/Person_Should.cs
+++ OOPsSolution/TDDUnitTestDemo/Person_Should.cs
@@ -22,6 +22,25 @@ namespace TDDUnitTestDemo
         // Attribute Title 
         // 1. Fact = which does one test and is usually set up and coded within the test
         // 2. Theory = allows for multiple test data strings apply to the same test
+
+        [Fact]
+        public void Create_an_Instance_Using_Default_Constructos()
+        {
+            // Arrange Area (setup)
+            string expectedfirstname = "unknown";
+            string expectedlastname = "unknown";
+
+            // Act Area (execution) ------ SUT means subject under test
+            Person sut = new Person();
+
+            // Assert Area (testing of the action)
+            sut.FirstName.Should().Be(expectedfirstname);
+            sut.LastName.Should().Be(expectedlastname);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count().Should().Be(0);
+
+        } // End of Create_an_Instance_With_First_And_Last_Name_Residence_With_NO_List_Of_Employment Test
+
         [Fact]
         public void Create_an_Instance_With_First_And_Last_Name_Residence_With_NO_List_Of_Employment()
         {
@@ -42,23 +61,6 @@ namespace TDDUnitTestDemo
 
         } // End of Create_an_Instance_With_First_And_Last_Name_Residence_With_NO_List_Of_Employment Test
 
-        [Fact]
-        public void Create_an_Instance_Using_Default_Constructos()
-        {
-            // Arrange Area (setup)
-            string expectedfirstname = "unknown";
-            string expectedlastname = "unknown";
-
-            // Act Area (execution) ------ SUT means subject under test
-            Person sut = new Person();
-
-            // Assert Area (testing of the action)
-            sut.FirstName.Should().Be(expectedfirstname);
-            sut.LastName.Should().Be(expectedlastname);
-            sut.Address.Should().BeNull();
-            sut.EmploymentPositions.Count().Should().Be(0);
-
-        } // End of Create_an_Instance_With_First_And_Last_Name_Residence_With_NO_List_Of_Employment Test
 
         [Fact]
         public void Change_FirstName_To_New_Name()

44762ac802a49964f4521e74f034758d0d87fbff
rruiz103 <raymondjohn.ruiz16@gmail.com>
2023-06-21 17:14:58 -0400

    Web App Update

--- WebAppSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ WebAppSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -9,8 +9,19 @@ namespace WebApp.Pages.Samples
         public double Num { get; set; }
 
         public string MassText { get;set; }
+
+        public int FavouriteCourse { get; set; } // using integer value for select
+
+        public string FavouriteCourseNoValueOnOption { get; set; } // using string value for select
+
+        public string FeedBack { get; set; }
+
         public void OnGet()
         {
         }
+
+        public void OnPostControlProcessing() 
+        { FeedBack = $"Number value is {Num}, MassText is {MassText}, Favourite Course with value is {FavouriteCourse}, " +
+                $"Favourite Course without value is {FavouriteCourseNoValueOnOption}"; }
     }
 }
